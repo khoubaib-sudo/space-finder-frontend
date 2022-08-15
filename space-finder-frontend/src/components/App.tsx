e54f7d41033002eafa93501758553a4f6199d30d
khoubaib-sudo <83957252+khoubaib-sudo@users.noreply.github.com>
2022-08-15 18:57:05 +0200

    Child components

--- space-finder-frontend/src/components/App.tsx
+++ space-finder-frontend/src/components/App.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { User } from '../model/Model'
+import { AuthService } from '../services/AuthService'
+import { Login } from './Login'
+
+
 interface AppState{
-  user: User
+  user: User | undefined
 }
 
+export class App extends React.Component<{},AppState>{
 
-
-export class App extends React.Component<{},{}>{
-
-
-
+  private authService: AuthService = new AuthService();
+ 
   render(){
     return(
-      <div>App from class works!!</div>
+      
+      <div>App from class works!!
+        <Login authService={this.authService}/>
+      </div>
     )
   }
 }
