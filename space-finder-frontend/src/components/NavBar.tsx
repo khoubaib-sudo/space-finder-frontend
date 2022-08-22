7a67e1682e51f1abaa090735d764afc1699999e4
khoubaib-sudo <83957252+khoubaib-sudo@users.noreply.github.com>
2022-08-22 18:11:45 +0200

    Navbar

--- space-finder-frontend/src/components/NavBar.tsx
+++ space-finder-frontend/src/components/NavBar.tsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { User } from "../model/Model";
+import { Link } from 'react-router-dom'
 
 
-
-export class NavBar extends React.Component{
+export class NavBar extends React.Component<{user: User | undefined}>{
     render(){
+        let loginLogOut: any
+        if (this.props.user) {
+            loginLogOut = <Link to='/logout'>{this.props.user.userName}</Link>
+        }else{
+            loginLogOut = <Link to='/login'>login</Link>
+        }
         return(
-            <div>
-                Welcome to profile page!
+            <div className = 'navbar'>
+                <Link to='/'> Home</Link>
+                <Link to='/profile'> Profile</Link>
+                {loginLogOut}
             </div>
         )
     }
