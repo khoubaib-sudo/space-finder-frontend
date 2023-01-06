cb122303090afec31d301cf2ffeff47dd236200c
khoubaib-sudo <83957252+khoubaib-sudo@users.noreply.github.com>
2023-01-06 12:23:43 +0100

    Update App.tsx

--- space-finder-frontend/src/components/App.tsx
+++ space-finder-frontend/src/components/App.tsx
@@ -1,53 +1,10 @@
 import React from 'react';
-import { User } from '../model/Model'
-import { AuthService } from '../services/AuthService'
-import { Login } from './Login'
-import { Router, Route, Routes} from 'react-router-dom'
-import history from '../utils/history';
-import { NavBar } from './NavBar';
-import { Home } from './Home';
-import { Profile } from './Profile';
 
+export class App extends React.Component{
 
-interface AppState{
-  user: User | undefined
-}
-
-export class App extends React.Component<{},AppState>{
-
-  private authService: AuthService = new AuthService();
-
-  constructor(props: any){
-    super(props)
-    this.setUser = this.setUser.bind(this)
-  }
-
-  private setUser(user : User){
-    this.setState({
-      user :user
-    })
-    console.log('setting the user!: ' + user);
-
-  }
- 
   render(){
     return(
-      
-      <div className = 'wrapper'>
-        <Router history={history}>
-          <div>
-            <NavBar user={this.state.user}/>
-            <Switch>
-              <Route exact path='/' component={Home}/>
-              <Route exact path='/login'>
-                <Login authService={this.authService} setUser={this.setUser}/>
-              </Route>
-              <Route exact path='/profile' component={Profile}/>
-            </Switch>
-          </div>
-
-        </Router>
-      </div>
+      <div></div>
     )
   }
 }
